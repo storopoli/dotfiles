0d12bb276ed8d34f88978f28167cfe076592316e
Jose Storopoli <jose@storopoli.io>
2023-04-01 07:59:00 -0300

    fix: telescope selectors and tab as nvim-cmp selectors

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -68,13 +68,13 @@ return {
 		local cmp_mappings = lsp.defaults.cmp_mappings({
 			["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
 			["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-			["<C-y>"] = cmp.mapping.confirm({ select = true }),
+			["<CR>"] = cmp.mapping.confirm({ select = true }),
 			["<C-Space>"] = cmp.mapping.complete(),
 		})
 
 		-- I like my tabs, but I should stop doing this
-		cmp_mappings["<Tab>"] = nil
-		cmp_mappings["<S-Tab>"] = nil
+		-- cmp_mappings["<Tab>"] = nil
+		-- cmp_mappings["<S-Tab>"] = nil
 
 		-- include latex symbols please
 		local cmp_sources = lsp.defaults.cmp_sources()
